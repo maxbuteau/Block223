93fccc86407064a8c286b2d96126e4cf100bc4ae
willbouch <williambouchardpei@gmail.com>
2019-02-17 12:03:14 -0500

    Setting up for iteration 3
    Modification to Block223.ump
    Adding Block223TransferObjects.ump
    Adding the empty controller class

--- ECSE223_P13/ca/mcgill/ecse223/block/application/Block223Application.java
+++ ECSE223_P13/ca/mcgill/ecse223/block/application/Block223Application.java
@@ -1,23 +1,35 @@
 package ca.mcgill.ecse223.block.application;
 
 import ca.mcgill.ecse223.block.model.Block223;
+import ca.mcgill.ecse223.block.model.UserRole;
 import ca.mcgill.ecse223.block.persistence.Block223Persistence;
 import ca.mcgill.ecse223.block.view.Block223Page;
 
 public class Block223Application {
 	
-	private static Block223 btms;
+	private static Block223 block223;
+	private static UserRole loggedUser;
 	
 	public static void main(String[] args) {
 		
 	}
 
-	public static Block223 getBtms() {
-		if (btms == null) {
+	public static Block223 getBlock223() {
+		if (block223 == null) {
 			// load model
-			btms = Block223Persistence.load();
+			block223 = Block223Persistence.load();
 		}
- 		return btms;
+ 		return block223;
 	}
+
+	public static UserRole getLoggedUser() {
+		return loggedUser;
+	}
+
+	public static void setLoggedUser(UserRole loggedUser) {
+		Block223Application.loggedUser = loggedUser;
+	}
+	
+	
 	
 }
--- ECSE223_P13/ca/mcgill/ecse223/block/controller/Block223Controller.java
+++ ECSE223_P13/ca/mcgill/ecse223/block/controller/Block223Controller.java
@@ -1,21 +1,82 @@
 package ca.mcgill.ecse223.block.controller;
 
-import ca.mcgill.ecse223.block.application.Block223Application;
-import ca.mcgill.ecse223.block.model.Admin;
-import ca.mcgill.ecse223.block.model.Ball;
-import ca.mcgill.ecse223.block.model.Block;
-import ca.mcgill.ecse223.block.model.Block223;
-import ca.mcgill.ecse223.block.model.BlockAssignment;
-import ca.mcgill.ecse223.block.model.Game;
-import ca.mcgill.ecse223.block.model.Level;
-import ca.mcgill.ecse223.block.model.Paddle;
-import ca.mcgill.ecse223.block.model.Player;
-import ca.mcgill.ecse223.block.model.User;
-import ca.mcgill.ecse223.block.model.UserRole;
-import ca.mcgill.ecse223.block.persistence.Block223Persistence;
+import java.util.List;
 
-public class Block223Controller {
+class Block223Controller {
 
-	
-	
+	// ****************************
+	// Modifier methods
+	// ****************************
+	public static void createGame(String name) throws InvalidInputException {
+	}
+
+	public static void setGameDetails(int nrLevels, int nrBlocksPerLevel, int minBallSpeedX, int minBallSpeedY,
+			Double ballSpeedIncreaseFactor, int maxPaddleLength, int minPaddleLength) throws InvalidInputException {
+	}
+
+	public static void deleteGame(String name) throws InvalidInputException {
+	}
+
+	public static void selectGame(String name) throws InvalidInputException {
+	}
+
+	public static void updateGame(String name, int nrLevels, int nrBlocksPerLevel, int minBallSpeedX, int minBallSpeedY,
+			Double ballSpeedIncreaseFactor, int maxPaddleLength, int minPaddleLength) throws InvalidInputException {
+	}
+
+	public static void addBlock(int red, int green, int blue, int points) throws InvalidInputException {
+	}
+
+	public static void deleteBlock(int id) throws InvalidInputException {
+	}
+
+	public static void updateBlock(int id, int red, int green, int blue, int points) throws InvalidInputException {
+	}
+
+	public static void positionBlock(int id, int level, int gridHorizontalPosition, int gridVerticalPosition)
+			throws InvalidInputException {
+	}
+
+	public static void moveBlock(int level, int oldGridHorizontalPosition, int oldGridVerticalPosition,
+			int newGridHorizontalPosition, int newGridVerticalPosition) throws InvalidInputException {
+	}
+
+	public static void removeBlock(int level, int gridHorizontalPosition, int gridVerticalPosition)
+			throws InvalidInputException {
+	}
+
+	public static void saveGame() throws InvalidInputException {
+	}
+
+	public static void register(String username, String playerPassword, String adminPassword)
+			throws InvalidInputException {
+	}
+
+	public static void login(String username, String password) throws InvalidInputException {
+	}
+
+	public static void logout() {
+	}
+
+	// ****************************
+	// Query methods
+	// ****************************
+	public static List<TOGame> getDesignableGames() {
+	}
+
+	public static TOGame getCurrentDesignableGame() {
+	}
+
+	public static List<TOBlock> getBlocksOfCurrentDesignableGame() {
+	}
+
+	public static TOBlock getBlockOfCurrentDesignableGame(int id) throws InvalidInputException {
+	}
+
+	public List<TOGridCell> getBlocksAtLevelOfCurrentDesignableGame(int level) throws InvalidInputException {
+	}
+
+	public static TOUserMode getUserMode() {
+	}
+
 }
