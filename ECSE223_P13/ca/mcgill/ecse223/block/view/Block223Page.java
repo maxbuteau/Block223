8db6a0eb02bc90b2d8575efc049cc0e66eb8b4b3
emmacona <emmanuelle.coutu-nadeau@mail.mcgill.ca>
2019-02-27 18:16:34 -0500

    toolbox, chosenBlock

--- ECSE223_P13/ca/mcgill/ecse223/block/view/Block223Page.java
+++ ECSE223_P13/ca/mcgill/ecse223/block/view/Block223Page.java
@@ -7,20 +7,27 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.List;
@@ -30,6 +37,8 @@ import ca.mcgill.ecse223.block.controller.*;
 import ca.mcgill.ecse223.block.controller.TOUserMode.Mode;
 
 public class Block223Page extends Application{
+
+	private static ChosenBlock chosenBlock;	
 
 	private Label loginError;
 	private Label registerError;
@@ -73,6 +82,14 @@ public class Block223Page extends Application{
 	private Button gameSelectionUpdateGameButton;
 	private Scene createGameScene;
 	private VBox createGameBox;
+
+	//TOOLBOX
+	private Button toolboxButton;
+	private VBox toolboxBox;
+	private Button toolboxDeleteButton;
+	private Button toolboxColorPickerButton;
+	private Scene toolboxScene;	
+	private static Pane selectedPane = new Pane();
 
 	private final double SCREEN_WIDTH = 500; // to be changed
 	private final double SCREEN_HEIGHT = 500; // to be changed
@@ -205,31 +222,31 @@ public class Block223Page extends Application{
 			}
 			registerError.setText("Password and Confirm Password must be the same");
 		});
-		
+
 		registerError = new Label();
 		registerError.setStyle("-fx-text-fill: #DC143C");
 
 		registerPane.getChildren().addAll(registerBox, registerButton, registerError);
 
-			Pane pane = new Pane();
-			Scene scene = new Scene(pane, SCREEN_WIDTH, SCREEN_HEIGHT);
-			buttonPressSound();
-			//change the values accordingly ^
-			primaryStage.setScene(loginScene);
-			primaryStage.setResizable(false);
-			primaryStage.show();
-			primaryStage.getIcons().add(new Image("ca/mcgill/ecse223/block/view/resources/logo.jpg"));
+		Pane pane = new Pane();
+		Scene scene = new Scene(pane, SCREEN_WIDTH, SCREEN_HEIGHT);
+		buttonPressSound();
+		//change the values accordingly ^
+		primaryStage.setScene(loginScene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		primaryStage.getIcons().add(new Image("ca/mcgill/ecse223/block/view/resources/logo.jpg"));
 
 
-			/* Louis' comment: Did a first draft of the QUIT label which can be found below.
-			 * Let me know if you can find a better style (font, colors, etc.) for it.
-			 */
-			Label quitLabel = new Label("QUIT");
-			quitLabel.setStyle("-fx-font:20 Garamond; -fx-padding:3px; -fx-text-fill: #DC143C; -fx-border-color:black;-fx-background-color:POWDERBLUE;-fx-font-weight:bold");
+		/* Louis' comment: Did a first draft of the QUIT label which can be found below.
+		 * Let me know if you can find a better style (font, colors, etc.) for it.
+		 */
+		Label quitLabel = new Label("QUIT");
+		quitLabel.setStyle("-fx-font:20 Garamond; -fx-padding:3px; -fx-text-fill: #DC143C; -fx-border-color:black;-fx-background-color:POWDERBLUE;-fx-font-weight:bold");
 	}
 
 	private void changeToGameSelectionScene(Stage primaryStage) {
-		
+
 		//SELECTION GAME
 		gameSelectionPane = new VBox(20);
 		gameSelectionScene = new Scene(gameSelectionPane, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -239,7 +256,7 @@ public class Block223Page extends Application{
 		gameSelectionButtonRow.setAlignment(Pos.BOTTOM_CENTER);
 		gameSelectionButtonRow.setPadding(new Insets(10, 10, 10, 10));
 		gameSelectionCreateGameButton = new Button("Create");
-		
+
 		gameSelectionCreateGameButton.setOnAction(e -> {
 			Stage createGameStage = new Stage();
 			createGameBox = new VBox(20);
@@ -255,7 +272,7 @@ public class Block223Page extends Application{
 					} catch (InvalidInputException e1) {
 						gameSelectionError.setText(e1.getMessage());
 					}
-					
+
 					refreshGameSelection();
 					gameSelectionCreateGameButton.setDisable(false);
 				}
@@ -280,7 +297,7 @@ public class Block223Page extends Application{
 		//List
 		gameSelectionList = new ListView<String>();
 		gameSelectionListData = FXCollections.observableArrayList();
-		
+
 		//error
 		gameSelectionError = new Label();
 		gameSelectionError.setStyle("-fx-text-fill: #DC143C");
@@ -291,7 +308,83 @@ public class Block223Page extends Application{
 
 		primaryStage.setScene(gameSelectionScene);
 	}
-	
+
+
+	private void setToolboxPane() {
+		Stage toolboxStage = new Stage();
+		toolboxBox = new VBox(20);
+		Label toolboxLabel = new Label("Block toolbox");
+		toolboxLabel.setTranslateX(SCREEN_WIDTH/6);
+
+		//Color picker
+		ColorPicker toolboxColorPicker = new ColorPicker();
+
+		//Slider worth
+		Slider toolboxWorthSlider = new Slider(1,1000, 500); //need to get min and max from block.java
+
+		//grid of blocks for toolbox
+		TilePane blockTilePane = new TilePane();
+		blockTilePane.setPadding(new Insets(30, 0, 30, 0));
+		blockTilePane.setVgap(20);
+		blockTilePane.setHgap(20);
+		blockTilePane.setAlignment(Pos.CENTER);
+		blockTilePane.setPrefColumns(4);
+		try {
+			List<TOBlock> toBlocks = Block223Controller.getBlocksOfCurrentDesignableGame();
+			for(TOBlock toBlock : toBlocks) {
+				//Getting block color for each block
+				Color blockColor = new Color(toBlock.getRed(), toBlock.getGreen(), toBlock.getBlue(), 100);
+				Pane toBlockPane = new Pane();
+				toBlockPane.setBackground(new Background(new BackgroundFill(blockColor, CornerRadii.EMPTY, Insets.EMPTY)));
+
+				//Getting worth and id for each block
+				Label toBlockId = new Label(String.valueOf(toBlock.getId()));
+				Label toBlockWorth = new Label(String.valueOf(toBlock.getPoints()));
+
+				//Adding color, worth, and id for each block in tile
+				blockTilePane.getChildren().addAll(toBlockPane, toBlockId, toBlockWorth);
+
+				//Adding shadow to selected block in toolbox
+				toBlockPane.setOnMouseClicked(ciao -> {
+					selectedPane.setEffect(null);
+					DropShadow dropShadow = new DropShadow();
+					dropShadow.setRadius(10);
+					dropShadow.setColor(Color.CHARTREUSE);
+					toBlockPane.setEffect(dropShadow);
+					selectedPane = toBlockPane;
+
+
+				}
+						);
+				//change selected block color
+				toolboxColorPicker.setOnMouseClicked(colorEvent -> {
+					BackgroundFill slectedColor = new BackgroundFill(toolboxColorPicker.getValue(),CornerRadii.EMPTY, Insets.EMPTY);
+					toBlockPane.setBackground(new Background(slectedColor));
+				});
+
+				//change selected block worth
+				toolboxWorthSlider.setOnMouseDragged(worthEvent -> {
+					toolboxWorthSlider.getValue();
+				});
+
+			}
+		} catch (InvalidInputException e1) {
+			e1.printStackTrace();
+		}
+
+		toolboxDeleteButton = new Button("Delete block");
+		toolboxColorPickerButton = new Button("Select block color");
+		toolboxBox.getChildren().addAll(blockTilePane, toolboxDeleteButton, toolboxColorPickerButton);
+
+		toolboxScene = new Scene(toolboxBox, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 6);
+		toolboxStage.setScene(toolboxScene);
+		toolboxStage.show();
+		toolboxStage.setAlwaysOnTop(true);
+		toolboxStage.setResizable(false);
+		toolboxButton.setDisable(true);
+	}
+
+
 	private void refreshGameSelection() {
 		gameSelectionList.getItems().clear();
 		try {
@@ -304,7 +397,7 @@ public class Block223Page extends Application{
 			e2.printStackTrace();
 		}
 	}
-	
+
 	private static String getResource(String res)
 	{
 		return ClassLoader.getSystemResource(res).toString();
@@ -317,4 +410,22 @@ public class Block223Page extends Application{
 		sound.setVolume(0.5);
 
 	}
+
+	public static ChosenBlock getChosenBlock() {
+		return chosenBlock;
+	}
+
+	private class ChosenBlock{
+		private int id;
+
+		public ChosenBlock(String id) {
+			this.id = Integer.parseInt(id);
+		}
+
+		public int getId() {
+			return id;
+		}
+
+	}
+
 }
