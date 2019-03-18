b1b78f63b157434a26f96e769248bf1624f906b1
willbouch <williambouchardpei@gmail.com>
2019-03-17 21:26:34 -0400

    Added isPublished check to getDesignableGames()

--- ECSE223_P13/ca/mcgill/ecse223/block/controller/Block223Controller.java
+++ ECSE223_P13/ca/mcgill/ecse223/block/controller/Block223Controller.java
@@ -531,7 +531,7 @@ public class Block223Controller {
 		for (Game game : games) {
 			Admin gameAdmin = game.getAdmin();
 
-			if (gameAdmin.equals(admin)) {
+			if (gameAdmin.equals(admin) && !game.isPublished()) {
 				TOGame toGame = new TOGame(game.getName(), game.getLevels().size(), game.getNrBlocksPerLevel(),
 						game.getBall().getMinBallSpeedX(), game.getBall().getMinBallSpeedY(),
 						game.getBall().getBallSpeedIncreaseFactor(), game.getPaddle().getMaxPaddleLength(),
