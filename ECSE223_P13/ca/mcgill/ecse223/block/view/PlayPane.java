abaa06c1c26423bee7c9d4663e3d36d51d389d94
willbouch <williambouchardpei@gmail.com>
2019-03-22 17:37:17 -0400

    Added skeleton class for PlayGame

---
+++ ECSE223_P13/ca/mcgill/ecse223/block/view/PlayPane.java
@@ -0,0 +1,17 @@
+package ca.mcgill.ecse223.block.view;
+
+public class PlayPane implements Block223PlayModeInterface {
+
+	@Override
+	public String takeInputs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void refresh() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
