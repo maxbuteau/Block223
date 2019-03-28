5d81173121c02c474c4f883338e30d3cf6c711c2
emmacona <emmanuelle.coutu-nadeau@mail.mcgill.ca>
2019-03-28 09:54:39 -0400

    J'ai commencé un peu louis !

--- ECSE223_P13/ca/mcgill/ecse223/block/view/HallOfFamePane.java
+++ ECSE223_P13/ca/mcgill/ecse223/block/view/HallOfFamePane.java
@@ -1,5 +1,46 @@
 package ca.mcgill.ecse223.block.view;
 
-public class HallOfFamePane {
+import java.util.List;
 
+import ca.mcgill.ecse223.block.controller.Block223Controller;
+import ca.mcgill.ecse223.block.controller.InvalidInputException;
+import ca.mcgill.ecse223.block.controller.TOGame;
+import ca.mcgill.ecse223.block.controller.TOHallOfFame;
+import ca.mcgill.ecse223.block.controller.TOHallOfFameEntry;
+import ca.mcgill.ecse223.block.controller.TOPlayableGame;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+
+public class HallOfFamePane extends VBox{
+
+	private static ObservableList<TOHallOfFameEntry> hallOfFameListData;
+	private static TableView hallOfFameList;
+	private VBox hallOfFameBox;
+	private Label hallOfFameLabel;
+
+	public HallOfFamePane() {
+		hallOfFameListData = FXCollections.observableArrayList();
+		hallOfFameList = new TableView<>();
+		hallOfFameList.setItems(hallOfFameListData);
+		
+		hallOfFameBox = new VBox(20);
+		hallOfFameLabel = new Label("Hall Of Fame");
+		hallOfFameBox.getChildren().addAll(hallOfFameLabel, hallOfFameList);	
+			
+	}
+	private static void refreshHallOfFamePane() {
+		hallOfFameList.getItems().clear();
+		try {
+			List<TOHallOfFameEntry> toHF = Block223Controller.getHallOfFame(0, 1).getEntries();
+			for (TOHallOfFameEntry to : toHF) {
+				hallOfFameListData.add(to);
+			}
+		} catch(InvalidInputException e) {
+			e.printStackTrace();
+		}
+	}
 }
